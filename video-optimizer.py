4c0106a7ca35232ef0ae631ee5851a75592c0d3c
ale <ale@quasar.us.es>
2017-03-17 12:06:42 +0100

    v4.15.5

--- video-optimizer.py
+++ video-optimizer.py
@@ -14,7 +14,7 @@ def generate_random_filename(prefix, suffix):
 
 # Constants:
 
-VERSION = 'v4.15.4'
+VERSION = 'v4.15.5'
 #APPEND_VERSION_TO_FILENAME = True
 APPEND_VERSION_TO_FILENAME = False
 VXT = ['mkv', 'mp4', 'm4v', 'mov', 'mpg', 'mpeg', 'avi', 'vob', 'mts', 'm2ts', 'wmv']
@@ -405,8 +405,8 @@ class MediaFile:
     if not args.x:
       options += ' --encoder-tune %s '%(encoder_tune)
     options += ' --encopts vbv-maxrate=%s:vbv-bufsize=%s'%(codec_maxrate, codec_bufsize)
-    if args.x:
-      quantizer = quantizer + 1
+    #if args.x:
+    #  quantizer = quantizer + 1
     options += ' --quality %d '%(quantizer)
     if args.noenc:
       #if args.d or args.v:
