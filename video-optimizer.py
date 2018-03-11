f0502eeb0d1d20b96332ede3cc4258f3a9e3c840
ale <ale@quasar.us.es>
2018-03-11 22:52:18 +0100

    v4.27.1

--- video-optimizer.py
+++ video-optimizer.py
@@ -14,7 +14,7 @@ def generate_random_filename(prefix, suffix):
 
 # Constants:
 
-VERSION = 'v4.27.0'
+VERSION = 'v4.27.1'
 #APPEND_VERSION_TO_FILENAME = True
 APPEND_VERSION_TO_FILENAME = False
 VXT = ['mkv', 'mp4', 'm4v', 'mov', 'mpg', 'mpeg', 'avi', 'vob', 'mts', 'm2ts', 'wmv', 'flv']
@@ -1055,7 +1055,7 @@ verify_software(NICE_BIN, True)
 verify_software(BIFTOOL_BIN, False)
 
 if args.upload:
-  c = 'git commit -a -m "%s" ; git push'%(VERSION)
+  c = 'cd /home/ale/bin/video-optimizer ; git commit -a -m "%s" ; git push'%(VERSION)
   execute_command(c)
 else:
   if args.input:
